6f4fae2748ec17149dfc0af01666e517ef3cab99
stephko1 <stephko@outlook.com>
2021-05-02 10:12:08 +0200

    tests

--- Android/app/src/main/java/com/example/android/MainActivity.java
+++ Android/app/src/main/java/com/example/android/MainActivity.java
@@ -4,15 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    // The TextView used to display the message inside the Activity.
+    private TextView mTextView;
 
+    // The EditText where the user types the message.
+    private EditText mEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCenter.start(getApplication(), "8cb0f829-9287-4f40-8e90-5f14ab8b9487",
                 Analytics.class, Crashes.class);
+
+
+        // Set the listeners for the buttons.
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String text = mEditText.getText().toString();
+
+        final int changeTextBtId = R.id.changeTextBt;
+        final int activityChangeTextBtnId = R.id.activityChangeTextBtn;
+
+        if (view.getId() == changeTextBtId) {
+            // First button's interaction: set a text in a text view.
+            mTextView.setText(text);
+        } else if (view.getId() == activityChangeTextBtnId) {
+            // Second button's interaction: start an activity and send a message to it.
+            Intent intent = com.example.android.ShowTextActivity
+                    .newStartIntent(this, text);
+            startActivity(intent);
+        }
     }
 }
---
+++ Android/app/src/main/java/com/example/android/ShowTextActivity.java
@@ -0,0 +1,42 @@
+package com.example.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+
+public class ShowTextActivity extends Activity {
+
+    // The name of the extra data sent through an {@link Intent}.
+    public final static String KEY_EXTRA_MESSAGE =
+            "com.example.android.testing.espresso.basicsample.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_text);
+
+        // Get the message from the Intent.
+        Intent intent = getIntent();
+        String message = Objects.toString(intent.getStringExtra(KEY_EXTRA_MESSAGE));
+
+        // Show message.
+        ((TextView)findViewById(R.id.show_text_view)).setText(message);
+    }
+
+    /**
+     * Creates an {@link Intent} for {@link ShowTextActivity} with the message to be displayed.
+     * @param context the {@link Context} where the {@link Intent} will be used
+     * @param message a {@link String} with text to be displayed
+     * @return an {@link Intent} used to start {@link ShowTextActivity}
+     */
+    static protected Intent newStartIntent(Context context, String message) {
+        Intent newIntent = new Intent(context, ShowTextActivity.class);
+        newIntent.putExtra(KEY_EXTRA_MESSAGE, message);
+        return newIntent;
+    }
+}
