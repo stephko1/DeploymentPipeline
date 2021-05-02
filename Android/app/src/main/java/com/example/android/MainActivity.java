4f0922ae9ff227f9ea8eb1f8a18853d3adaf4b23
stephko1 <stephko@outlook.com>
2021-05-02 09:19:22 +0200

    vs app center

--- Android/app/src/main/java/com/example/android/MainActivity.java
+++ Android/app/src/main/java/com/example/android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "8cb0f829-9287-4f40-8e90-5f14ab8b9487",
+                Analytics.class, Crashes.class);
     }
 }
