91e1356f2ad52baeaa045cf44f3b7279a2dae3fe
stephko1 <stephko@outlook.com>
2021-05-03 13:46:00 +0200

    Add files via upload

---
+++ ios/DeploymentPipeline/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DeploymentPipeline
+//
+//  Created by Stephan Kouch on 03.05.21.
+//  Copyright © 2021 Stephan Kouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "Hello World!"
+    }
+
+
+}
+
